2a40390a392816f35ed453b550e46c835dff874c
mayk789 <mayk789@gmail.com>
2015-03-08 22:00:07 -0500

    Created assignment operator

--- Screencast_Expressions/Expressions/js/script.js
+++ Screencast_Expressions/Expressions/js/script.js
@@ -2,4 +2,8 @@
 Week 2 Screencast: Expressions
  */
 
-alert("Javascript works!")
+//alert("Javascript works!")
+
+var a = 2; //sets variable with 2
+a = a +3; //assignment operator, adds 3 to a
+console.log(a);
