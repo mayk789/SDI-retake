e7bc87b285baf15650a34daaf1ee6010c0b92e73
mayk789 <mayk789@gmail.com>
2015-03-11 00:15:40 -0500

    Added var area
    Added var area which calculates var width by var height.

--- Screencast/PromptandAlert/js/script.js
+++ Screencast/PromptandAlert/js/script.js
@@ -7,3 +7,6 @@
 
 var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width.");
 var height = prompt("Please enter the height.");
+var area = width * height; //calculates info of width & height
+
+console.log(area); // prints to the console
