30f86f3affdf035e09d97b7059186c2df1184a1e
mayk789 <mayk789@gmail.com>
2015-03-11 22:51:19 -0500

    Created variables
    Created variables for the new problem: Slices of Pie part 1

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -10,3 +10,9 @@ var dogYears = humanYears * 7;// calculates dog years by human years
 
 
 console.log("Sparky is " + humanYears + " human years old which is " + dogYears + " dog years old."); // prints info to the console
+
+//Slice of Pie part 1
+var slicesPerPizza = prompt("How many slices are there per pizza?"); //prompts user to enter number of slices per pizza
+var people = prompt("How many will be attending?"); //prompts user to enter number of people attending
+var pizza = "How many pizzas will be ordered?";//prompts user to enter the number of pizzas that will be ordered
+
