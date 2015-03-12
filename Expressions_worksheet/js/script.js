aae830428df2d4ed88b310936be1f1657607212c
mayk789 <mayk789@gmail.com>
2015-03-11 23:45:31 -0500

    Created folder, comments
    Made folders and files for assignment. Also included necessary comments
    at beginning of js file.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -13,7 +13,7 @@ var dogYears = humanYears * 7;// calculates dog years by human years
 console.log("Sparky is " + humanYears + " human years old which is " + dogYears + " dog years old."); // prints info to the console
 */
 
-
+/*
 //Slice of Pie part 1
 var slicesPerPizza = prompt("How many slices are there per pizza?"); //prompts user to enter number of slices per pizza
 var people = prompt("How many will be attending?"); //prompts user to enter number of people attending
@@ -22,3 +22,11 @@ var slicesTotal = (pizza * slicesPerPizza); //calculates total number of slices
 var personEats = slicesTotal / people; //calculates the number of slices that each person gets
 
 console.log("Each person ate " + personEats + " slices of pizza at the party.")//prints number of slices of pizza each person ate to the console.
+*/
+
+//Slice of Pie part 2
+var slicesPerPizza = prompt("How many slices are there per pizza?"); //prompts user to enter number of slices per pizza
+var people = prompt("How many will be attending?"); //prompts user to enter number of people attending
+var pizza = prompt("How many pizzas will be ordered?"); //prompts user to enter the number of pizzas that will be ordered
+var slicesTotal = (pizza * slicesPerPizza); //calculates total number of slices
+var personEats = slicesTotal / people; //calculates the number of slices that each person gets
---
+++ Yang_MayKou_Expressions_Assignment/js/script.js
@@ -0,0 +1,7 @@
+/*
+May Kou Yang
+SDI Section 2
+MDV2330-O
+Week 2
+Expressions Assignment
+ */
