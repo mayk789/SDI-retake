3b9560de331f50f1bcc81b7ffe931767a8705977
mayk789 <mayk789@gmail.com>
2015-03-11 22:19:17 -0500

    Added calculation
    Added variable which calculates the number of dog years using the
    information entered by the user.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -6,3 +6,6 @@
 //Problem: Calculating how old Sparky is in dog years based on actual age.
 
 var humanYears = prompt("How old is Sparky in human years?"); //prompts user to enter number
+var dogYears = humanYears * 7;// calculates dog years by human years
+
+
