1985a96876f3c3d255d9d56bf8586286cfbedfb1
mayk789 <mayk789@gmail.com>
2015-03-11 23:05:46 -0500

    Printed to console
    Made script that prints number of pizza each person ate to the console.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -3,6 +3,7 @@
  */
 //Activity - Expression worksheet
 
+/*
 //Problem: Calculating how old Sparky is in dog years based on actual age.
 
 var humanYears = prompt("How old is Sparky in human years?"); //prompts user to enter number
@@ -10,11 +11,15 @@ var dogYears = humanYears * 7;// calculates dog years by human years
 
 
 console.log("Sparky is " + humanYears + " human years old which is " + dogYears + " dog years old."); // prints info to the console
+*/
+
 
 //Slice of Pie part 1
 var slicesPerPizza = prompt("How many slices are there per pizza?"); //prompts user to enter number of slices per pizza
 var people = prompt("How many will be attending?"); //prompts user to enter number of people attending
-var pizza = "How many pizzas will be ordered?"; //prompts user to enter the number of pizzas that will be ordered
-var slicesTotal = pizza / slicesPerPizza; //Performs calculations to show how many slices there are going to be overall
+var pizza = prompt("How many pizzas will be ordered?"); //prompts user to enter the number of pizzas that will be ordered
+var slicesTotal = (pizza * slicesPerPizza); //calculates total number of slices
+console.log(slicesTotal);
 var personEats = slicesTotal / people; //calculates the number of slices that each person gets
 
+console.log("Each person ate " + personEats + " slices of pizza at the party.")
