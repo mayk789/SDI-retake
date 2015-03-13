85344f16f9d95dc86e11ff1525326aa9566ed9d6
mayk789 <mayk789@gmail.com>
2015-03-12 20:06:43 -0500

    Made prompt and print
    Created a prompt and print to console for numberOfLoads variable.

--- Yang_MayKou_Expressions_Assignment/js/script.js
+++ Yang_MayKou_Expressions_Assignment/js/script.js
@@ -9,10 +9,16 @@ Expressions Assignment
 //alert("Javascript is working");
 
 //Variables
-var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var numberOfLoads;
-var washCost;
-var dryCost;
-var costOfLaundry = ((numberOfLoads * washCost) + (numberOfLoads * dryCost));
-var weeklyDeal = dayOfWeek[4];
-    dayOfWeek[4]=  costOfLaundry - (numberOfLoads * dryCost);
+var numberOfLoads; //Variable for the number of loads of laundry a person washes.
+var washCostPrice = [1, 1.50]; //Array
+var dryCost; //Variable which will contain the cost of using the drying machine.
+
+
+//Prompts
+numberOfLoads = prompt("Please enter the number of loads of laundry you do in a week.");
+//washCost = prompt("Select which wash cycle you use. Please enter the correlating number as shown: \n0 for Light Wash \n1 for Regular Wash \n2 for Heavy Duty Wash ");
+//dryCost = prompt("Enter how much it costs to run the drying machine for 1 cycle.")
+
+
+//Outputs
+console.log("You wash " + numberOfLoads + " loads of laundry in a week.")
