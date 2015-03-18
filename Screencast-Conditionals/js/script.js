2df37b80cd531f69de68ccca53e6779ada81306d
mayk789 <mayk789@gmail.com>
2015-03-18 00:58:06 -0500

    Entered in...
    Entered in condition and also code performed if the condition is true

--- Screencast-Conditionals/js/script.js
+++ Screencast-Conditionals/js/script.js
@@ -3,4 +3,14 @@ Week 3
 Screen cast: Conditionals
  */
 
-alert("Javascript is working. :)")
+//alert("Javascript is working. :)")
+
+//Variables
+
+var oldEnough = true;
+
+//if the child is old enough, print to the console, "you can ride!"
+if(oldEnough){
+    //code performed if condition it true
+    console.log("You can ride the coaster!");
+}
