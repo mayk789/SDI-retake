06f673bfe9b7ff5bd66f917b560dba20ac4670d1
mayk789 <mayk789@gmail.com>
2015-03-18 23:49:46 -0500

    Vars and Conditions
    Created variable and conditions. I’ve got 2 “if else” conditions &
    instead of one statement printed to console, there are 2

--- Screencast-Conditionals/js/script.js
+++ Screencast-Conditionals/js/script.js
@@ -1,26 +1,25 @@
 /*
 Week 3
 Screen cast: Conditionals
-Else If
+Logical Operators
  */
 
 //alert("Javascript is working. :)")
 
 //Variables
 
-var kidHeight = 48;
-var minHeight = 48;
-var wParentHeight = 45; //height of kid with parent
+var budget = 300;
+var iPhonePrice = 199.99;
+var paycheck = 200;
 
-//if the child is old enough, print to the console, "you can ride!"
-//if kid is over 48 inches in height
-if(kidHeight > minHeight){
-    //you can ride
-    console.log("You can ride the coaster!");
-}else if(kidHeight > wParentHeight){
-    //you can ride with parent present
-    console.log("You can ride but only with a parent present");
+//if price of phone is less than budget AND if paycheck os over 300.
+if(iPhonePrice < budget){
+    console.log("We can buy the phone!");
 }else{
-    //sorry, you have growing to do.
-    console.log("Sorry kid, you've got some growing to do first!");
+    console.log("No phone for you!");
+}
+if(paycheck > 300){
+    console.log("We can buy the phone!");
+}else{
+    console.log("No phone for you!");
 }
