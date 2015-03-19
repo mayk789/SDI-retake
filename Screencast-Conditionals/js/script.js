bbe818c6bbd40cf939245c7868951bf7cb397116
mayk789 <mayk789@gmail.com>
2015-03-18 23:30:09 -0500

    Created else if
    Created “else if” conditional and statement

--- Screencast-Conditionals/js/script.js
+++ Screencast-Conditionals/js/script.js
@@ -1,6 +1,7 @@
 /*
 Week 3
 Screen cast: Conditionals
+Else If
  */
 
 //alert("Javascript is working. :)")
@@ -9,13 +10,17 @@ Screen cast: Conditionals
 
 var kidHeight = 48;
 var minHeight = 48;
+var wParentHeight = 45; //height of kid with parent
 
 //if the child is old enough, print to the console, "you can ride!"
 //if kid is over 48 inches in height
 if(kidHeight > minHeight){
-    //code performed if condition it true
+    //you can ride
     console.log("You can ride the coaster!");
+}else if(kidHeight > wParentHeight){
+    //you can ride with parent present
+    console.log("You can ride but only with a parent present");
 }else{
-    //code performed if condition is false
+    //sorry, you have growing to do.
     console.log("Sorry kid, you've got some growing to do first!");
 }
