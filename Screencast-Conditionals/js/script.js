44f756aa9ce0b88aa31b4f2254890be3a2bfb4ea
mayk789 <mayk789@gmail.com>
2015-03-18 23:17:28 -0500

    Added another condition
    Added another if statement and condition.

--- Screencast-Conditionals/js/script.js
+++ Screencast-Conditionals/js/script.js
@@ -9,11 +9,13 @@ Screen cast: Conditionals
 
 var kidHeight = 47;
 var minHeight = 48;
-var sneakerLift = 2;
 
 //if the child is old enough, print to the console, "you can ride!"
 //if kid is over 48 inches in height
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight > minHeight){
     //code performed if condition it true
     console.log("You can ride the coaster!");
+}
+if(kidHeight < minHeight){
+    console.log("Sorry kid, you've got some growing to do first!");
 }
