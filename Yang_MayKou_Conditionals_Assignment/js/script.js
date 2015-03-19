4318c6284448dafbd46aeb80ba5239bb3cd5b972
mayk789 <mayk789@gmail.com>
2015-03-19 18:44:44 -0500

    Variable
    Entered vaiables

--- Yang_MayKou_Conditionals_Assignment/js/script.js
+++ Yang_MayKou_Conditionals_Assignment/js/script.js
@@ -5,4 +5,17 @@ SDI Section 02
  Week 3
  Conditionals Assignment
  */
-alert("Javascript is working.")
+//alert("Javascript is working.")
+
+//Variables
+var dailyCalGoal = [1000, 1500, 2000, 2500];
+var breakfastCal;
+var lunchCal;
+var dinnerCal;
+var calBurned;
+var dailyCalIntake;
+var sliceOfPie = 400;
+//Prompts
+
+
+//Outputs
