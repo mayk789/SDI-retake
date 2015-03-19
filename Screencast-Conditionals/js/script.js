58f3447a6215fdcd219fb3dfe257d4dc6804e378
mayk789 <mayk789@gmail.com>
2015-03-18 23:51:55 -0500

    && operator
    Used “&&” (“and”) operator to add second conditional to the first one.

--- Screencast-Conditionals/js/script.js
+++ Screencast-Conditionals/js/script.js
@@ -13,12 +13,7 @@ var iPhonePrice = 199.99;
 var paycheck = 200;
 
 //if price of phone is less than budget AND if paycheck os over 300.
-if(iPhonePrice < budget){
-    console.log("We can buy the phone!");
-}else{
-    console.log("No phone for you!");
-}
-if(paycheck > 300){
+if(iPhonePrice < budget && paycheck > 300){
     console.log("We can buy the phone!");
 }else{
     console.log("No phone for you!");
