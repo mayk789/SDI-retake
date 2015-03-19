874e58d7db42f2d321d54cbf80f9dfd55be2bae5
mayk789 <mayk789@gmail.com>
2015-03-18 23:09:55 -0500

    Added variable
    Added variables and changed condition. Included use of “>” sign.

--- Screencast-Conditionals/js/script.js
+++ Screencast-Conditionals/js/script.js
@@ -7,11 +7,12 @@ Screen cast: Conditionals
 
 //Variables
 
-var oldEnough = false;
+var kidHeight = 50;
+var minHeight = 48;
 
 //if the child is old enough, print to the console, "you can ride!"
-if(oldEnough){
+//if kid is over 48 inches in height
+if(kidHeight > minHeight){
     //code performed if condition it true
     console.log("You can ride the coaster!");
 }
-console.log("What comes after.");
