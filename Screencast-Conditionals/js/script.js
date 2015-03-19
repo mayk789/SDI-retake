78fcf802b42411e98ce640a2d6858ebd241d548c
mayk789 <mayk789@gmail.com>
2015-03-19 00:26:52 -0500

    Ternary
    Got rid of the original if conditional statement using ternary operator
    ( the ? mark)

--- Screencast-Conditionals/js/script.js
+++ Screencast-Conditionals/js/script.js
@@ -20,9 +20,12 @@ var age = 11;
 var book;
 
 //if child is under 10, they get "Green Eggs and Ham", otherwise they get "The Time Machine"
+/*
 if(age<10){
     book = "Green Eggs and Ham";
 }else{
     book = "Time Machine";
 }
+*/
+book = (age <10) ? "Green Eggs and Ham" : "Time Machine";
 console.log(book);
