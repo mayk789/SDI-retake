e880afc2191cf470c4b7f3f6da016ef7e6a4ba86
mayk789 <mayk789@gmail.com>
2015-03-19 20:25:52 -0500

    If else statement and else if
    Created “if else” & “else if” statements

--- Yang_MayKou_Conditionals_Assignment/js/script.js
+++ Yang_MayKou_Conditionals_Assignment/js/script.js
@@ -8,14 +8,36 @@ SDI Section 02
 //alert("Javascript is working.")
 
 //Variables
-var dailyCalGoal = [1000, 1500, 2000, 2500];
-var breakfastCal;
-var lunchCal;
-var dinnerCal;
+
+var calIntake;
 var calBurned;
-var dailyCalIntake;
-var sliceOfPie = 400;
+var totalCalLeft;
+var female = true;//Boolean which indicates gender of user.
+var dailyCalGoal = [2000, 2500]; //Average recommended calorie intake for women is 2,000. Whereas, for men, it is 2500.
+var sliceOfPie = 400; //number of calories in a slice of apple pie
+
 //Prompts
 
+var calIntake = prompt("Please enter in the total amount of calories you consumed today.");//prompts user for a value
+calIntake = Number(calIntake); //makes sure this will be a number
+if (calIntake > 0) {
+    //runs this if calIntake is greater than 0
+    console.log("Your calorie intake for today is " + calIntake + ".");
+} else {
+    calIntake = prompt("Please enter total number of calories you consumed today. Your intake should not be 0.");
+    console.log("Okay, your calorie intake is " + calIntake + " for today.");
+};
 
-//Outputs
+var calBurned = prompt("Enter the number of calories that you used today.");
+calBurned = Number(calBurned);
+if (calBurned >= 1500){
+    console.log("You burned " + calBurned + " calories today. Way to go!");
+}else if (calBurned < 1500, calBurned > 400){
+    console.log("You used " + calBurned + " calories today. Not bad!");
+}else{
+    console.log("You burned only " + calBurned + " calories. Come one you can do more than that!")
+}
+
+//Confirm
+female = confirm("Are you a female? Click \"Ok\" for true or \"Cancel\" for false.")
+
