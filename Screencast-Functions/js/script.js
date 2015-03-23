c2b5e1026ed2a5281f9937e042c75f57249740ee
mayk789 <mayk789@gmail.com>
2015-03-22 23:50:21 -0500

    Entered 1st & 2nd function
    Screencast follow along

--- Screencast-Functions/js/script.js
+++ Screencast-Functions/js/script.js
@@ -5,4 +5,13 @@ Screen cast: Functions
 
 //alert("Javascript is working.");
 
+function outptMsg(){
+    console.log("Hello World");
+}
 
+function calcArea(){
+    var width= 20;
+    var height= 30;
+    var area= width * height;
+    console.log(area);
+}
