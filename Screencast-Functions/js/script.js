7d0039d3aa938cbe8693d184a8b93fa7db9162b3
mayk789 <mayk789@gmail.com>
2015-03-23 00:11:25 -0500

    Difference between variable scopes
    If same variable names are used, one will be “scoped” to the function
    and the other one will be “scoped” to the larger document or other
    function if it is there. Basically, it will be scoped to depending on
    where it’s location is.

--- Screencast-Functions/js/script.js
+++ Screencast-Functions/js/script.js
@@ -5,17 +5,18 @@ Screen cast: Functions
 
 //alert("Javascript is working.");
 
+var width= 5;
+
 function outptMsg(){
     console.log("Hello World");
 }
 
-function calcArea(){
-    var width= 20;
-    var height= 30;
-    var area= width * height;
-    console.log(area);
+function calcArea() {
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    //console.log(area);
 }
 
 calcArea();
-calcArea();
-calcArea();
+console.log(width);
