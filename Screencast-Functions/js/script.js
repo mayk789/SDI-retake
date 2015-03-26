e33de030057f94e4c923a0da7fe9f27a503d75c0
mayk789 <mayk789@gmail.com>
2015-03-25 22:55:23 -0500

    Do while loops
    Do while loops seems to work just like regular while loops. While loops
    are more common that do while loops.

--- Screencast-Functions/js/script.js
+++ Screencast-Functions/js/script.js
@@ -9,3 +9,12 @@ while (b > 0){ //this block checks condition
     console.log(b + " kegs on the wall");
     b--; //this increments or decrements the index
 }
+
+var c = 10;
+
+do{
+    console.log(c + ' kegs on the wall.');
+    c--;
+}
+while(c > 0);
+
