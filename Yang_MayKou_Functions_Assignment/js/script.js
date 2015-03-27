479abdfe6e4fa69d43e93660fa3c048e87547199
mayk789 <mayk789@gmail.com>
2015-03-26 20:47:37 -0500

    Created first function

--- Yang_MayKou_Functions_Assignment/js/script.js
+++ Yang_MayKou_Functions_Assignment/js/script.js
@@ -6,8 +6,16 @@ Week 4
 Functions Assignment
  */
 
+/* NOTE TO SELF:
+ Florida lottery has 6 random numbers ranging from 1-53,
+ Powerball lottery has 5 random numbers ranging from 1-59 & 1 powerball number ranging from 1-35
+ */
+
+
 //alert("Javascript is working!");
 
+
+/*
 //Variable
 var lotteryGame; //Lottery game of choice.
 
@@ -17,16 +25,39 @@ var lotteryGame = prompt("Please enter which lottery game you would like to purc
 
 
 if (lotteryGame === "Florida"){//runs it "Florida" is entered
-    console.log("So you wish to purchase a " + lotteryGame + " lottery ticket!");//this prints to the console if "Florida" is entered
+    console.log("So you wish to purchase a " + lotteryGame + " lottery ticket. Good luck!");//this prints to the console if "Florida" is entered
 
 }else if (lotteryGame === "Powerball"){//runs if "Powerball" is entered
-
-    console.log("Buying a " + lotteryGame + "? Good luck!");//this prints to the console if "Powerball" is entered
+    console.log("Buying a " + lotteryGame + " ticket? Good luck on it!");//this prints to the console if "Powerball" is entered
 
 }else{//runs if anything other than "Florida" or "Powerball" has been entered by user
     lotteryGame = prompt("Looks like you did not enter Florida or Powerball. Please enter one of the two.");//Prompts the user again to enter either "Florida" or "Powerball"
-    console.log("Glad you came to a decision. You'll be getting your " + lotteryGame + " ticket.");//user input is printed to the console
+    console.log("Glad you came to a decision. You'll be getting your " + lotteryGame + " ticket in a moment.");//user input is printed to the console
+}
+
+*/
+
+//Function
+
+function randomNumberDrawn(min, max, digit){
+
+    var array = [];//creates the array
+
+//Loop
+    for (var a = 0; a < digit; a++){//defines starting point for "var a" as 0. As long as the number of loops is less than "digit", keeps running. 0 is not included in lottery numbers ++ to guarantee that we do not get 0.
+        var lotteryNumber = Math.round()(Math.random() * (max - min) + min);
+
+        array[a] = lotteryNumber;
+
+    }
+
 }
 
 
 //Main code
+
+//Florida lottery
+randomNumberDrawn(1, 59, 6);
+
+//Powerball lottery
+randomNumberDrawn(1, 35, 5);
