385b21b2a737f8848999376b54d07925e825b4b1
mayk789 <mayk789@gmail.com>
2015-03-26 19:36:58 -0500

    Created first var and prompt
    Created variable and prompts for which lottery game they would like to
    get a ticket for.

--- Yang_MayKou_Functions_Assignment/js/script.js
+++ Yang_MayKou_Functions_Assignment/js/script.js
@@ -3,10 +3,29 @@ May Kou Yang
 SDI Section 02
 MDV2330-O
 Week 4
-Functions Assigment
+Functions Assignment
  */
 
 //alert("Javascript is working!");
 
-//Variables
+//Variable
+var lotteryGame; //Lottery game of choice.
 
+
+//Prompt
+var lotteryGame = prompt("Please enter which lottery game you would like to purchase a ticket for. Enter either Florida or Powerball."); //Prompts the user to enter which lottery game they would like to purchase a ticket for.
+
+
+if (lotteryGame === "Florida"){//runs it "Florida" is entered
+    console.log("So you wish to purchase a " + lotteryGame + " lottery ticket!");//this prints to the console if "Florida" is entered
+
+}else if (lotteryGame === "Powerball"){
+
+    console.log("Buying a " + lotteryGame + "? Good luck!");
+
+}else{
+    lotteryGame = prompt("Looks like you did not enter Florida or Powerball. Please enter one of the two.");
+    console.log("Glad you came to a decision. You'll be getting a " + lotteryGame + " ticket.");
+}
+
+//Main code
