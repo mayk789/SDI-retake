4758ea3ae596b166400adaf66252ed4c13e02af5
mayk789 <mayk789@gmail.com>
2015-03-26 23:09:58 -0500

    Final code run through
    Final code run through. Seems to perform fine.

--- Yang_MayKou_Functions_Assignment/js/script.js
+++ Yang_MayKou_Functions_Assignment/js/script.js
@@ -32,14 +32,34 @@ if (lotteryGame === "Florida"){//runs if "Florida" is entered
     console.log("Buying a " + lotteryGame + " ticket? Good luck on it!");//this concatenation prints to the console if "Powerball" is entered
 
 }else{
-    lotteryGame = prompt("Looks like you did not enter Florida or Powerball. Please enter one of the two.");//Prompts the user again to enter either "Florida" or "Powerball"
-    console.log("Glad you came to a decision. You'll be getting your " + lotteryGame + " ticket in a moment.");//user input is printed to the console
+    lotteryGame = prompt("You need to choose and enter either Florida or Powerball. Please do so in the space.");
+    console.log("Glad that you made a decision. You will be getting your " + lotteryGame + " ticket in a moment.");
+
 }
+
 
 
 //Function
 
-function whichLottery
+/*function whichLottery(lotteryGame){
+
+    var chancesGiven = 1;
+
+    while (lotteryGame === " "){
+
+        lotteryGame = prompt("You can not leave this blank. Please enter one of the two choices you are given.");
+
+        chancesGiven++;
+
+        if(chancesGiven === 3){
+            console.log("You were already given 3 more chances to enter your decision. No more for you.");
+        }
+    }
+}
+
+Can not get the above to run properly
+
+*/
 
 function randomNumberDrawn(min, max, digit){//settings for parameters
 
@@ -65,7 +85,7 @@ if(lotteryGame === "Florida"){ //Florida lottery
 }else if(lotteryGame === "Powerball"){ //Powerball lottery
     powerballTicket = randomNumberDrawn(1, 59, 5);//sets min to 1, max to 59, & sets the digits/loops to 5. These are the first 5 numbers of the Powerball
     powerballNum = randomNumberDrawn(1, 35, 1);//sets min to 1, max to 35, & sets it at 1 digit/loop. This is the last Powerball number
-    console.log("These are your numbers: " + powerballTicket + "," +powerballNum);
+    console.log("These are your numbers: " + powerballTicket + " & " +powerballNum);
 }
 
 
